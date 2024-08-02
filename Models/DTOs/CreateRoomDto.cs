03880f779da31650bad142178244223d834b8cef
Cristian Hernandez <chinorubikguitar@hotmail.com>
2024-08-01 18:12:38 -0600

    feature(add some validations to create a room)

--- Models/DTOs/CreateRoomDto.cs
+++ Models/DTOs/CreateRoomDto.cs
@@ -6,10 +6,12 @@ namespace hotel_clone_api.Models.DTOs
     {
         [Required]
         [MinLength(5)]
+        [MaxLength(255)]
         public string Name { get; set; }
 
         [Required]
         [MinLength(5)]
+        [MaxLength(2000)]
         public string Description { get; set; }
 
         [Required]
@@ -17,10 +19,11 @@ namespace hotel_clone_api.Models.DTOs
 
         [Required]
         [MinLength(5)]
+        [MaxLength(255)]
         public string Characteristics { get; set; }
 
         [Required]
-        [Range(100, 5000)]
+        [Range(100, 500000)]
         public decimal Price { get; set; }
     }
 }
